3d4a68745e454194be682b0f0b2e717d590cfd30
SebastiaanSimons <sebastiaansimons@outlook.com>
2021-12-25 15:16:44 +0100

    change to font awesome icons

--- src/sub-components/Level.svelte
+++ src/sub-components/Level.svelte
@@ -1,43 +1,40 @@
 <script lang="ts">
     export let level: number;
 </script>
+
 <div>
-<img class="icon" alt="blue dot" src="assets/icons/circle-blue.svg" />
+    <i class="fas fa-circle icon main-color" />
+    {#if level > 1}
+        <i class="fas fa-circle icon main-color" />
+    {:else}
+        <i class="fas fa-circle icon" />
+    {/if}
 
-{#if level > 1}
-    <img class="icon" alt="blue dot" src="assets/icons/circle-blue.svg" />
-{:else}
-    <img class="icon" alt="black dot" src="assets/icons/circle-black.svg" />
-{/if}
+    {#if level > 2}
+        <i class="fas fa-circle icon main-color" />
+    {:else}
+        <i class="fas fa-circle icon" />
+    {/if}
 
-{#if level > 2}
-    <img class="icon" alt="blue dot" src="assets/icons/circle-blue.svg" />
-{:else}
-    <img class="icon" alt="black dot" src="assets/icons/circle-black.svg" />
-{/if}
+    {#if level > 3}
+        <i class="fas fa-circle icon main-color" />
+    {:else}
+        <i class="fas fa-circle icon" />
+    {/if}
 
-{#if level > 3}
-    <img class="icon" alt="blue dot" src="assets/icons/circle-blue.svg" />
-{:else}
-    <img class="icon" alt="black dot" src="assets/icons/circle-black.svg" />
-{/if}
-
-{#if level > 4}
-    <img class="icon" alt="blue dot" src="assets/icons/circle-blue.svg" />
-{:else}
-    <img class="icon" alt="black dot" src="assets/icons/circle-black.svg" />
-{/if}
+    {#if level > 4}
+        <i class="fas fa-circle icon main-color" />
+    {:else}
+        <i class="fas fa-circle icon" />
+    {/if}
 </div>
 
 <style>
     .icon {
-        width: 17px;
-        color: #00bfff;
+        width: 12px;
         vertical-align: middle;
     }
-    img {
-        margin: 0px;
-        padding: 0px;
-        float: left;
+    .main-color {
+        color: var(--main-color);
     }
 </style>
